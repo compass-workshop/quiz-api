3f9cfcd766e6a0ab1087d172ad9bc83802701281
Sarthak Jain <sarthak.jain@comprotechnologies.com>
2023-09-14 17:15:37 +0530

    updated entity to dto

--- src/test/dto/question.dto.ts
+++ src/test/dto/question.dto.ts
@@ -1,4 +1,4 @@
-export interface Question {
+export class Question {
   id: string;
   text: string;
   type: string;
--- src/test/entities/test.entity.ts
+++
@@ -1,7 +0,0 @@
-import { Question } from './question.entity';
-
-export interface Test {
-  id: string;
-  name: string;
-  questions: Question[];
-}
