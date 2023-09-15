50f10b95c847014908574290a308d794fd36cd15
Sarthak Jain <sarthak.jain@comprotechnologies.com>
2023-09-15 16:56:37 +0530

    updated prisma-db-layer

--- src/db-client/db-client.factory.ts
+++ src/db-client/db-client.factory.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaDBClient } from './prisma-client/prisma-db-client';
+import { constants } from 'src/contants';
 
 @Injectable()
 export class DBClientFactory {
   constructor(private prismaDBClient: PrismaDBClient) {}
 
   getDatabaseClient(flag: string) {
-    if (flag === 'Prisma') {
+    if (flag === constants.PRISMA) {
       return this.prismaDBClient;
     }
   }
