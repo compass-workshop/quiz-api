0f08ad94563bb2a275c0d71fcca7132e67f359e8
SusantoMandal <86454870+SusantoMandal@users.noreply.github.com>
2023-09-21 15:14:35 +0530

    updated misspell text

--- src/evaluation/evaluate-score.service.ts
+++ src/evaluation/evaluate-score.service.ts
@@ -43,7 +43,7 @@ export class EvaluateScoreService {
           );
 
           if (!currQuestion) {
-            throw new NotFoundException('Question not found in text in DB');
+            throw new NotFoundException('Question not found in test in DB');
           }
           const marksObtained = this.getScore(currQuestion, submittedAnswer);
           const evaluatedAnswer: EvaluatedAnswer = {
